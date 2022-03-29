1542e59e4a5acdc93c13cdd85940948c19dba0fb
Jamison Ordway <jamisonordway@gmail.com>
2022-03-29 12:23:39 -0600

    Adds aggregate stability estimate

--- app/models/lyric_line.rb
+++ app/models/lyric_line.rb
@@ -1,4 +1,30 @@
 class LyricLine < ApplicationRecord
   belongs_to :section, optional: true
-  enum stability: ['Very Balanced', 'Balanced', 'Unbalanced', 'Very Unbalanced']
+  enum stability: ['None', 'Balanced', 'Unbalanced']
+
+  def stability_estimate
+    if content.count_syllables.even?
+      'Balanced'
+    else
+      'Unbalanced'
+    end
+  end
+
+  def self.aggregate_stability_estimate
+    counts = group(:stability).count
+    balanced = counts['Balanced'].to_f
+    unbalanced = counts['Unbalanced'].to_f
+    percentage_balanced = balanced / (balanced + unbalanced)
+    return 'N/A' if (balanced.nil?) && (unbalanced.nil?)
+
+    if percentage_balanced < 0.25
+      return 'Very Unbalanced'
+    elsif (percentage_balanced >= 0.25) && (percentage_balanced < 0.51)
+      return 'Unbalanced'
+    elsif (percentage_balanced >= 0.51) && (percentage_balanced < 0.75)
+      return 'Balanced'
+    else percentage_balanced > 0.75
+      return 'Very Balanced'
+    end
+  end
 end
--- spec/models/lyric_line_spec.rb
+++ spec/models/lyric_line_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe LyricLine do
+  describe 'instance methods' do
+    describe 'stability_estimate' do
+      before(:each) do
+        @line_1 = LyricLine.create(content: 'this is a lyric')
+        @line_2 = LyricLine.create(content: 'even')
+        @line_3 = LyricLine.create(content: 'odd')
+        @line_4 = LyricLine.create(content: 'longer lyric that is even')
+        @line_5 = LyricLine.create(content: 'longer lyric that is odd')
+      end
 
+      it 'guesses the line stability based on syllable count' do
+        expect(@line_1.stability_estimate).to eq('Unbalanced')
+        expect(@line_2.stability_estimate).to eq('Balanced')
+        expect(@line_3.stability_estimate).to eq('Unbalanced')
+        expect(@line_4.stability_estimate).to eq('Balanced')
+        expect(@line_5.stability_estimate).to eq('Unbalanced')
+      end
+    end
+  end
+
+  describe 'class methods' do
+    describe 'aggregate_stability_estimate' do
+      before(:each) do
+        @line_1 = LyricLine.create(content: 'this is a lyric', stability: 2)
+        @line_2 = LyricLine.create(content: 'even', stability: 1)
+        @line_3 = LyricLine.create(content: 'odd', stability: 2)
+      end
+
+      it 'guesses the overall stability of a combination of lyric lines' do
+        expect(LyricLine.aggregate_stability_estimate).to eq('Unbalanced')
+      end
+    end
+  end
 end
